736562c93cbf923cc3059cd4e827d82967cc76c0
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2024-09-19 10:12:22 +0600

    set up tailwind and daysi with create a react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text-5xl bg-red-600'>Vite + React</h1>
+      <button className="btn">Button</button>
+    </>
+  )
+}
+
+export default App
